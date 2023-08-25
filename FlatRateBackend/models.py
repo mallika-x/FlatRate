75da35a6e08fa67e5638fcedb39276ddf35f9a87
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-08-26 01:08:50 +1000

    Fixed user table & migrations

--- FlatRateBackend/models.py
+++ FlatRateBackend/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-GMAILMAXLEN = 320
+#https://www.rfc-editor.org/errata/eid1690 -- 320 but mariadb mandates <=255
+EMAILMAXLEN     = 255
+
+#https://www.bdm.vic.gov.au/births/naming-your-child/naming-restrictions#toolong
+FNAMESMAXLEN    = 38
+SNAMEMAXLEN     = 38
 
 class User(models.Model):
     """I like this better than the default"""
-    email   = models.EmailField(max_length = GMAILMAXLEN, primary_key = True)
-    fnames  = models.CharField(max_length = FNAMES_MAXLEN)
-    snames  = models.CharField(max_length = SNAMES_MAXLEN)
+    email   = models.EmailField(max_length = EMAILMAXLEN, primary_key = True)
+    fnames  = models.CharField(max_length = FNAMESMAXLEN)
+    snames  = models.CharField(max_length = SNAMEMAXLEN)
     pii     = models.FileField() # see if this works without upload_to param
     photo   = models.ImageField()   # this too
