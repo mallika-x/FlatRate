9db96689360ad6aba1cf23031c7c3edf65afee63
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-08-26 00:57:45 +1000

    User model

--- FlatRateBackend/models.py
+++ FlatRateBackend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+GMAILMAXLEN = 320
+
+class User(models.Model):
+    """I like this better than the default"""
+    email   = models.EmailField(max_length = GMAILMAXLEN, primary_key = True)
+    fnames  = models.CharField(max_length = FNAMES_MAXLEN)
+    snames  = models.CharField(max_length = SNAMES_MAXLEN)
+    pii     = models.FileField() # see if this works without upload_to param
+    photo   = models.ImageField()   # this too
