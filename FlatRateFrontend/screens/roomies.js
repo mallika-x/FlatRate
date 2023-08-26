92a1407bb3f5f0c8aeea68ebf55cdc246c236296
Mallika <s4586114@student.uq.edu.au>
2023-08-26 20:58:38 +1000

    Added dummy message screen

--- FlatRateFrontend/screens/roomies.js
+++ FlatRateFrontend/screens/roomies.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import { SafeAreaView, iew, Text, TextInput, Button, StyleSheet } from 'react-native';
-import styles from "../styles"
-import Header from '../components/Header'
+import { Text, ScrollView, SafeAreaView, StyleSheet, View } from 'react-native'
+import { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import PrimaryButton from '../components/PrimaryButton';
+import SecondaryButton from '../components/SecondaryButton';
+import globalStyles from "../styles"
+import endpoints from '../endpoints';
+
+
+const styles = StyleSheet.create({
+  button: {
+    height:61,
+    marginVertical:8
+  }
+});
 
 export default ({navigation}) => {
-        return (
-            <SafeAreaView style={[styles.engageBgContainer, {justifyContent:"flex-start"}]}>
-                <Header text="Roomies" navigation={navigation} />
-            </SafeAreaView>
-        );
-};
+    return (
+        <SafeAreaView style={[globalStyles.engageBgContainer, {justifyContent:"flex-start"}]}>
+          <Header text="Messages" navigation={navigation}/>
+          <View style={{alignSelf: "stretch", margin: 50, justifyContent: "space-between"}}>
+            <SecondaryButton text="Send new message" size={22} style={styles.button} onPress={() => {}} />
+            <PrimaryButton text="Add new roomie" size={22} style={styles.button} onPress={() => {}} />
+          </View>
+        </SafeAreaView>
+      );
+}
